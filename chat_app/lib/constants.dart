2263bc734477cf5924f01826482440e360656bc6
kareemmuhamedd <kareemmuhamed2g@gmail.com>
2024-02-20 18:25:53 +0200

    delete trash folder

--- chat_app/lib/constants.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-const kPrimaryColor = Color(0xff2B475E);
-const kMessagesCollection='messages';
-const kMessage='message';
-const kCreatedAt='createdAt';
-const kId='id';
-const kError='<><><> error is <><><>';
